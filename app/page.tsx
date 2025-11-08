5d9659bb44c2bdfb2d2db0a3d0e9775cc8524ee0
DM-Coderas <danielmagidov@gmail.com>
2025-11-07 19:06:04 -0800

    playing around

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ const IconMap: {
 
 export default function Home() {
 	return (
-		<div className="font-geist-sans antialiased bg-gray-950 text-gray-200 min-h-screen">
+		<div className="font-geist-sans antialiased bg-gray-900 text-gray-200 min-h-screen">
 			<div className="max-w-5xl mx-auto p-4 sm:p-6 lg:p-8">
 
 				{/* Header/Nav */}
